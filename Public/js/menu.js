497c59343c21eb9f9a2382ef60e0f90c7f06a4cc
gbrpelizza <gbrpelizza@gmail.com>
2022-08-16 16:01:01 -0300

    el menu se cierra al elejir una opcion

--- Public/js/menu.js
+++ Public/js/menu.js
@@ -1,17 +1,33 @@
-var boton=document.querySelector(".header_icono")
-var menu=document.querySelector(".header__navigation")
-var cont=0
-console.log(boton)
-boton.onclick =function(){
-  if(cont%2==0){
-  menu.classList.add("header__navigation--active")
-  menu.classList.remove("header__navigation--disabled")
-}
+var boton = document.querySelector(".header_icono")
 
-else{
+var menu = document.querySelector(".header__navigation")
+
+var link = document.querySelectorAll(".navigation__link");
+
+var cont = 0
+
+
+boton.onclick = function () {
+  if (cont % 2 == 0) {
+    menu.classList.add("header__navigation--active")
+    menu.classList.remove("header__navigation--disabled")
+  }
+
+  else {
     menu.classList.remove("header__navigation--active")
     menu.classList.add("header__navigation--disabled")
   }
-  cont ++
-  }
-console.log(boton)
+  cont++
+}
+
+for (let i = 0; i < link.length; i++) {
+  const element = link[i];
+  link[i].onclick = function () {
+    menu.classList.remove("header__navigation--active")
+    menu.classList.add("header__navigation--disabled")
+  }  
+}
+
+
+
+
--- Public/js/slider.js
+++ Public/js/slider.js
@@ -1,6 +1,6 @@
 new Glider(document.querySelector('.glider'), {
     slidesToShow: 4,
-    slidesToScroll: 1,
+    slidesToScroll: 3,
     autoPlay:true,
     draggable: true,
     dots: '.dots',
